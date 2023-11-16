31791f7d45fff890063a830a0ff00109b8c702d0
blues <blues@LAPTOP-FO43AURS>
2023-11-17 02:53:37 +0900

    gui

--- project0/src/Pingpong_Game.java
+++ project0/src/Pingpong_Game.java
@@ -2,8 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
-public class Game2 {
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+public class Pingpong_Game {
     private JFrame frame;
     private GamePanel gamePanel;
     private int player1X, player1Y, player2X, player2Y, ballX, ballY;
@@ -12,6 +14,7 @@ public class Game2 {
     private double initialBallSpeedX, initialBallSpeedY;
     private boolean start=true;
     private boolean upPressed, downPressed,wPressed,sPressed;
+    private BufferedImage player1Image;
     public void move() {
         if (upPressed && player2Y > 0) {
         	player2Y -= playerSpeed;
@@ -26,31 +29,38 @@ public class Game2 {
         	player1Y += playerSpeed;
         }
     }
-    public Game2() {
+    public Pingpong_Game(JFrame j) {
     	
         frame = new JFrame("Game2");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 400);
+        frame.setSize(1300, 800);
         frame.setResizable(false);
-        playerSizeX = 10 ;
-        playerSizeY=50;
-        ballSize = 20;
+        frame.setLocationRelativeTo(null);
+        playerSizeX = 1 ;
+        playerSizeY=100;
+        ballSize = 40;
         playerSpeed = 12;
         initialBallSpeedX = 0;
         initialBallSpeedY = 0;
         ballSpeedMultiplier = 1.01;
 
-        player1X = 50;
+        player1X = 100;
         player1Y = frame.getHeight() / 2 - playerSizeY / 2;
 
-        player2X = frame.getWidth() - playerSizeX - 50;
-        player2Y = frame.getHeight() / 2 - (playerSizeY-50);
+        player2X = frame.getWidth() - playerSizeX - 100;
+        player2Y = frame.getHeight() / 2 - (playerSizeY-100);
 
         ballX = frame.getWidth() / 2 - ballSize / 2;
         ballY = frame.getHeight() / 2 - ballSize / 2;
 
         gamePanel = new GamePanel();
         frame.add(gamePanel);
+        try {
+            player1Image = ImageIO.read(getClass().getResource("blackhole.jpg"));
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         frame.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {}
@@ -60,8 +70,8 @@ public class Game2 {
                 int keyCode = e.getKeyCode();
                 if (e.getKeyChar() == ' '&&start) {
                 	start=false;
-                	initialBallSpeedX = 4;
-                    initialBallSpeedY = 2;
+                	initialBallSpeedX = 6;
+                    initialBallSpeedY = 3;
                 }
                 if (keyCode == KeyEvent.VK_W) {
                     setWPressed(true);
@@ -98,8 +108,8 @@ public class Game2 {
             if (ballX < player1X + playerSizeX && ballY > player1Y && ballY+ballSize < player1Y + playerSizeY) {
                 initialBallSpeedX = (int) (initialBallSpeedX * ballSpeedMultiplier);
                 initialBallSpeedX = -initialBallSpeedX;
-                initialBallSpeedX++;
-                initialBallSpeedY+=0.5f;
+                initialBallSpeedX+=2;
+                initialBallSpeedY++;
             } else if (ballX + ballSize > player2X && ballY > player2Y && ballY+ballSize < player2Y + playerSizeY) {
                 initialBallSpeedX = (int) (initialBallSpeedX * ballSpeedMultiplier);
                 initialBallSpeedX = -initialBallSpeedX;
@@ -107,12 +117,24 @@ public class Game2 {
 
             // 공이 화면 경계에 부딪혔을 때 처리
             if (ballX <= 0) {
-            	// p1 true
-                frame.setVisible(false);
+            	if(!start) {
+            		JOptionPane.showMessageDialog(null, "Player 1가 이겼습니다!");
+            		start=true;
+                    frame.setVisible(false);
+                	j.setVisible(true);
+            	}
+
+
             }
             else if (ballX + ballSize+ballSize >= frame.getWidth()) {
-            	// p2 true
-            	frame.setVisible(false);
+            	if(!start) {
+            		JOptionPane.showMessageDialog(null, "Player 2가 이겼습니다!");
+            		start=true;
+                    frame.setVisible(false);
+                	j.setVisible(true);
+            	}
+                
+
             }
             
             if (ballY <= 0 || ballY + ballSize+ballSize+ballSize >= frame.getHeight()) {
@@ -141,28 +163,26 @@ public class Game2 {
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
-            g.setColor(Color.RED);
-            g.fillRect(player1X, player1Y, playerSizeX, playerSizeY);
+            g.drawImage(player1Image, 0, 0,1300, 800, this);
+            g.drawImage(player1Image, player1X, player1Y,playerSizeX, playerSizeY, this);
 
-            g.setColor(Color.BLUE);
-            g.fillRect(player2X, player2Y, playerSizeX, playerSizeY);
+            g.drawImage(player1Image, player2X, player2Y,playerSizeX, playerSizeY, this);
 
-            g.setColor(Color.YELLOW);
-            g.fillOval(ballX, ballY, ballSize, ballSize);
+            g.drawImage(player1Image, ballX, ballY, ballSize, ballSize, this);
+            
+
             if(start) {
             	String text = "준비완료 : spacebar클릭"; // 출력할 글자
-                int x = 100; // X 좌표
-                int y = 50; // Y 좌표
+                int x = 440; // X 좌표
+                int y = 300; // Y 좌표
 
                 // 글자 그리기
+                g.drawImage(player1Image, x, y-40,100, 40, this);
                 g.setColor(Color.BLACK); // 글자 색상 설정
-                g.setFont(new Font("맑음 고딕", Font.BOLD, 30)); // 글자 폰트 설정
+                g.setFont(new Font("맑음 고딕", Font.BOLD, 40)); // 글자 폰트 설정
                 g.drawString(text, x, y);
             }
-            //p1 p2 true에 따라 각자 승리 글자 출력
+
         }
-    }
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new Game2());
     }
 }
